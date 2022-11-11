619315e064b29ffd9f3a0138b1cf040a04a50176
ValeryParfenov <91310408+ValeryParfenov@users.noreply.github.com>
2022-11-11 22:27:40 +0300

    Реализован конструктор класса Matrix, принимающий на вход имя файла, из которого нужно считать матрицу.

--- cem3/project/main.cpp
+++ cem3/project/main.cpp
@@ -2,28 +2,75 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <vector>
+#include <memory>
 
-void read(std::string filename){
-    std::ifstream f;
-    f.open(filename);
-    std::string string;
-    int value;
-    while(!f.eof())
-    {
-        getline(f, string);
-        if(string.empty()){
-            continue;
+template <typename VarType>
+class Matrix final{
+public:
+    std::vector<std::vector<VarType>> matrix;
+    std::unique_ptr<int[]> is_var_free; // ели i эллемент массива 1, то переменная свободна, если 0 - не свободна
+    unsigned Height = 0; // размеры матрицы
+    unsigned Width = 0;
+    Matrix(){};
+public:
+    // Конструктор, считывающий матрицу из файла
+    Matrix(std::string filename){
+        std::ifstream input;
+        input.open(filename);
+        std::string string;
+        VarType value;
+        while(!input.eof()){
+            getline(input, string);
+            if(string.empty()){
+                continue;
+            }
+            matrix.push_back(std::vector<VarType>()); // пушим новую строчку в матрицу и начинаем заполнять
+            std::stringstream buffer(string);
+            if(Height == 0){
+                while(buffer >> value){
+                    Width += 1;
+                    matrix[0].push_back(value);
+                }
+                if(Width == 0){
+                    std::cerr << "invalid input (Mat[0x0])";
+                    exit(1);
+                }
+            } // если заполняемая строка первая
+            else{
+                unsigned current_width = 0;
+                while(buffer >> value){
+                    matrix[Height].push_back(value);
+                    current_width += 1;
+                    if(current_width > Width){
+                        std::cerr << "invalid input";
+                        exit(1);
+                    }
+                }
+                if(current_width < Width){
+                    std::cerr << "invalid input";
+                    exit(1);
+                }
+            }
+            Height += 1;
         }
-        std::stringstream buffer(string);
-        while(buffer >> value){
-            std::cout << "0";
-        }
-        std::cout << string << std::endl;
+        is_var_free.reset(new int[Width] {0});
+        input.close();
     }
-    f.close();
-}
+
+    void print() const{
+        for(int i = 0; i < Height; i ++){
+            for(int j = 0; j < Width; j++){
+                std::cout << matrix[i][j] << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+};
+
 
 int main() {
-    read("input.txt");
+    Matrix<int> m("input.txt");
+    //m.print();
     return 0;
 }
